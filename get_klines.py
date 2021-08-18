91c6623aafc06c9a091d3c5c9a666e136324e135
nuzhnoff <nuzhnoff@gmail.com>
2021-08-18 17:24:52 +0300

    Create get_klines.py

---
+++ get_klines.py
@@ -0,0 +1,18 @@
+from binance import Client
+#временно вынес секретные ключи в отдельный файла
+import config_binance_api
+#TODO Необходимо разобраться с модулем configparser
+'''import configparser
+# Загрузка ключей из файла config
+config = configparser.ConfigParser()
+config.read_file(open('home/files-notes/config-binance-api.cfg'))
+api_key = config.get('BINANCE', 'actual_api_key')
+secret_key = config.get('BINANCE', 'actual_secret_key')'''
+#подгружаем ключи
+config_binance_api.init()
+
+api_key = config_binance_api.actual_api_key
+api_secret = config_binance_api.actual_secret_key
+client = Client(api_key, api_secret)
+candles = client.get_klines(symbol='BTCUSDT',limit=1, interval=Client.KLINE_INTERVAL_1MINUTE)
+print(candles)
